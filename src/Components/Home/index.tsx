b3cdce402ddd456e32b54c09c6d7b4cc53f22773
FelipeESchmidt <schmidtfe2@gmail.com>
2022-04-29 23:07:15 -0300

    feat: bettering home Component

--- src/Components/Home/index.tsx
+++ src/Components/Home/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Header from '../Header';
-import { links } from '../Header/index.constants';
 import MainInfo from '../MainInfo';
 
 import * as S from './index.styles';
@@ -9,7 +8,7 @@ import * as S from './index.styles';
 function Home() {
   return (
     <>
-      <S.StyledHome id={links[0].toLocaleLowerCase()} />
+      <S.StyledHome />
       <Header />
       <MainInfo />
     </>
