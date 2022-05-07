fe107757660200c7d793de331e689c1a15565e1c
FelipeESchmidt <schmidtfe2@gmail.com>
2022-05-07 10:40:49 -0300

    feat: adding the lifetime info

--- src/Components/Lifetime/index.constants.ts
+++ src/Components/Lifetime/index.constants.ts
@@ -23,27 +23,46 @@ export const lifetime: Array<LifeTimeProps> = [
   {
     year: 2017,
     title: 'Started programming',
+    description: `My coding journey started with an electrical subject on C programming, and it was at that moment that I found exactly what I love to do,
+      what makes me happy and makes me feel free.`,
+  },
+  {
+    year: 2018,
+    title: 'College',
+    description: `After high school I had to choose what I would do with my life, keeping in mind that I found programming something that I really like, I 
+      chose Computer Science as my college course.`,
+  },
+  {
+    year: 2018,
+    title: 'Graduated',
+    description: 'I got graduated at high school with a technical course in electrical.',
+  },
+  {
+    year: 2018,
+    title: 'First Job',
     description:
-      'My coding journey started with an electrical subject on C programming, and it was at that moment that I found exactly what I love to do, what makes me happy and makes me feel free.',
+      'My professional career started at PCE Engineer as intern, working with electronics.',
   },
   {
-    year: 2018,
-    title: 'Graduated',
-    description: 'I got graduated at high school with a technical course in electrical.',
+    year: 2019,
+    title: 'Programmer',
+    description: `After almost a year working with electronics I received an offer at the same company to work as an automation developer and 2D equipment image maker.
+      Stacks: C++, TIA Portal and Corel Draw.`,
   },
   {
-    year: 2018,
-    title: 'Graduated',
-    description: 'I got graduated at high school with a technical course in electrical.',
+    year: 2020,
+    title: 'Web development',
+    description: `After one year and a few months working as automation developer I decided to quit and focus in web development, so I started studying about it.
+      My initial focus was on Front-End with basic HTML, CSS and Javascript websites. After that completed I started to study about Back-End with PHP and SQL.`,
   },
   {
-    year: 2018,
-    title: 'Graduated',
-    description: 'I got graduated at high school with a technical course in electrical.',
+    year: 2021,
+    title: 'React Scholarship',
+    description: `Within my college I was able to do a 3 months scholarship with Compass UOL to learn React JS development.`,
   },
   {
-    year: 2018,
-    title: 'Graduated',
-    description: 'I got graduated at high school with a technical course in electrical.',
+    year: 2021,
+    title: 'Front-End Developer',
+    description: `After my scholarship I received a proposal to work as a Front-End developer at Compass UOL. Stacks: React, Typescript, Jest, Git and Storybook.`,
   },
 ];
