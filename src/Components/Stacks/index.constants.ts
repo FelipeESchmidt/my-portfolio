860a1cdca1b37efa1314b59024fa8b2cdd9deec0
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-03-15 19:44:49 -0300

    feat: bettering and updating stacks

--- src/Components/Stacks/index.constants.ts
+++ src/Components/Stacks/index.constants.ts
@@ -4,16 +4,16 @@ interface StackProps {
 }
 
 export const stacks: Array<StackProps> = [
-  { name: 'HTML', goodAt: 95 },
-  { name: 'Javascript', goodAt: 90 },
-  { name: 'React', goodAt: 85 },
-  { name: 'SASS', goodAt: 80 },
-  { name: 'Typecript', goodAt: 80 },
+  { name: 'HTML', goodAt: 100 },
+  { name: 'Javascript', goodAt: 95 },
+  { name: 'React', goodAt: 90 },
+  { name: 'Typescript', goodAt: 85 },
+  { name: 'SASS', goodAt: 85 },
   { name: 'Jest', goodAt: 70 },
+  { name: 'StoryBook', goodAt: 70 },
   { name: 'React Native', goodAt: 70 },
-  { name: 'PHP', goodAt: 50 },
-  { name: 'NodeJS', goodAt: 50 },
-  { name: 'SQL', goodAt: 40 },
-  { name: 'Puppet', goodAt: 30 },
-  { name: 'Python', goodAt: 30 },
+  { name: 'PHP', goodAt: 60 },
+  { name: 'NodeJS', goodAt: 60 },
+  { name: 'SQL', goodAt: 60 },
+  { name: 'Python', goodAt: 40 },
 ];
